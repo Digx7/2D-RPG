8e406ee687875775e6d0f97b43119a318dda648f
Digx7 <37136301+Digx7@users.noreply.github.com>
2025-02-20 20:12:41 -0800

    Can now get path from navmesh

--- Assets/Scripts/TileMapNavMesh.cs
+++ Assets/Scripts/TileMapNavMesh.cs
@@ -3,6 +3,7 @@ using UnityEngine.Tilemaps;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Utils;
 
 [RequireComponent(typeof(Tilemap))]
 public class TileMapNavMesh : MonoBehaviour
@@ -10,6 +11,9 @@ public class TileMapNavMesh : MonoBehaviour
     [SerializeField] private List<TileNavMeshPoint> points;
     [SerializeField] private BoundsInt area;
     private Tilemap tilemap;
+
+    public Vector3Int TestStartSpot;
+    public Vector3Int TestEndSpot;
 
     public void Awake()
     {
@@ -21,6 +25,21 @@ public class TileMapNavMesh : MonoBehaviour
         Color color = tilemap.color;
         color.a = 0f;
         tilemap.color = color;
+
+
+        List<TileNavMeshPoint> path = new List<TileNavMeshPoint>();
+        if(GetPath(TestStartSpot, TestEndSpot, ref path))
+        {
+            Debug.Log("Found Path and traversing");
+            foreach (TileNavMeshPoint point in path)
+            {
+                Debug.Log("" + point.position);
+            }
+        }
+        else
+        {
+            Debug.Log("Failed to find path");
+        }
     }
 
     [ContextMenu("Bake")]
@@ -48,6 +67,16 @@ public class TileMapNavMesh : MonoBehaviour
     {
         points.Clear();
         Debug.Log("TileMapNavMesh: cleared points");
+    }
+
+    public TileNavMeshPoint GetPointAt(Vector3Int location)
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            if(location == points[i].position) return points[i];
+        }
+
+        return null;
     }
 
     private void GatherAllPoints()
@@ -105,10 +134,89 @@ public class TileMapNavMesh : MonoBehaviour
         return false;
     }
 
-    // public bool GetPath(Vector3Int startLocation, Vector3Int endLocation, ref List<TileNavMeshPoint> path)
-    // {
-    //     if(!IsValidLocation(startLocation) || !IsValidLocation(endLocation)) return false;
+    public List<TileNavMeshPoint> GetNeighbors(Vector3Int location)
+    {
+        List<TileNavMeshPoint> neighbors = new List<TileNavMeshPoint>();
+        
+        Vector3Int left = location + Vector3Int.left;
+        Vector3Int upLeft = location + Vector3Int.up + Vector3Int.left;
+        Vector3Int up = location + Vector3Int.up;
+        Vector3Int upRight = location + Vector3Int.up + Vector3Int.right;
+        Vector3Int right = location + Vector3Int.right;
+        Vector3Int downRight = location + Vector3Int.down + Vector3Int.right;
+        Vector3Int down = location + Vector3Int.down;
+        Vector3Int downLeft = location + Vector3Int.down + Vector3Int.left;
 
-    //     path.Clear();
-    // }
+        if(IsValidLocation(left)) neighbors.Add(GetPointAt(left));
+        if(IsValidLocation(upLeft)) neighbors.Add(GetPointAt(upLeft));
+        if(IsValidLocation(up)) neighbors.Add(GetPointAt(up));
+        if(IsValidLocation(upRight)) neighbors.Add(GetPointAt(upRight));
+        if(IsValidLocation(right)) neighbors.Add(GetPointAt(right));
+        if(IsValidLocation(downRight)) neighbors.Add(GetPointAt(downRight));
+        if(IsValidLocation(down)) neighbors.Add(GetPointAt(down));
+        if(IsValidLocation(downLeft)) neighbors.Add(GetPointAt(downLeft));
+
+        return neighbors;
+    }
+
+
+    public bool GetPath(Vector3Int startLocation, Vector3Int endLocation, ref List<TileNavMeshPoint> path)
+    {
+        if(!IsValidLocation(startLocation) || !IsValidLocation(endLocation)) return false;
+
+        path.Clear();
+
+        TileNavMeshPoint startPoint = GetPointAt(startLocation);
+        TileNavMeshPoint endPoint = GetPointAt(endLocation);
+
+        // A star
+        PriorityQueue<TileNavMeshPoint, int> frontier = new PriorityQueue<TileNavMeshPoint, int>();
+        frontier.Enqueue(startPoint, 0);
+        Dictionary<TileNavMeshPoint, TileNavMeshPoint> cameFrom = new Dictionary<TileNavMeshPoint, TileNavMeshPoint>();
+        Dictionary<TileNavMeshPoint, int> costSoFar = new Dictionary<TileNavMeshPoint, int>();
+        cameFrom[startPoint] = null;
+        costSoFar[startPoint] = 0;
+
+        while(frontier.Count  != 0)
+        {
+            TileNavMeshPoint current = frontier.Dequeue();
+
+            if (current == endPoint)
+                break;
+            
+            List<TileNavMeshPoint> neighbors = GetNeighbors(current.position);
+            foreach (TileNavMeshPoint next in neighbors)
+            {
+                int newCost = costSoFar[current] + (int)Vector3Int.Distance(current.position, next.position);
+                if(!costSoFar.ContainsKey(next) || newCost < costSoFar[next])
+                {
+                    costSoFar[next] = newCost;
+                    int priority = newCost + (int)Vector3Int.Distance(next.position, endPoint.position);
+                    frontier.Enqueue(next, priority);
+                    cameFrom[next] = current;
+                }
+            }
+        }
+
+        
+        // runs backwards to get path
+        if(cameFrom.ContainsKey(endPoint))
+        {
+            TileNavMeshPoint x = endPoint;
+
+            path.Add(x);
+
+            while (x != startPoint)
+            {
+                x = cameFrom[x];
+                path.Add(x);
+            }
+
+            path.Reverse();
+            return true;
+        }
+        else return false;
+
+        
+    }
 }
