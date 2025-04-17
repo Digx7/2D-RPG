3b37610bc4c0c363c58d2a13756a674cfdbc0379
Digx7 <37136301+Digx7@users.noreply.github.com>
2025-04-17 12:52:40 -0700

    Updated all doors

--- Assets/Scripts/SceneHelpers/SceneChannelRaiser.cs
+++ Assets/Scripts/SceneHelpers/SceneChannelRaiser.cs
@@ -4,7 +4,19 @@ using UnityEngine.Events;
 public class SceneChannelRaiser : MonoBehaviour
 {
     [SerializeField] private SceneChannel channelToRaise;
-    [SerializeField] private SceneData m_Data;
+    [SerializeField] private string chapter;
+    [SerializeField] private string area;
+    [SerializeField] private string subArea;
+    [SerializeField] private SceneContext context;
+
+    private SceneData m_Data;
+
+    public void Awake()
+    {
+        m_Data.sceneName = chapter + "_" + area;
+        if(subArea != "") m_Data.sceneName = m_Data.sceneName + "_" + subArea;
+        m_Data.context = context;
+    }
 
     public void Raise()
     {
