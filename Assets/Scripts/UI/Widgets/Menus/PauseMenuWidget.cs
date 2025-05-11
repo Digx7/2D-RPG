24c99229dea30a4839b7d45a7fd25dd2e18731db
Digx7 <37136301+Digx7@users.noreply.github.com>
2025-05-11 10:42:05 -0700

    Started on pause menu structure
    Still need to get navigation working

--- Assets/Scripts/UI/Widgets/Menus/PauseMenuWidget.cs
+++ Assets/Scripts/UI/Widgets/Menus/PauseMenuWidget.cs
@@ -1,10 +1,30 @@
 using UnityEngine;
+using System;
+
+[System.Serializable]
+public enum PauseMenuMainTabs
+{
+    Journal, Equipment, Stats, Abilities, Inventory, System
+}
+
+[System.Serializable]
+public enum PauseMenuJournalTabs
+{
+    Maps, Enemies, Locations, Groups, Hints
+}
+
+[System.Serializable]
+public enum PauseMenuInventoryTabs
+{
+    Armor, Weapons, Douments, KeyItems
+}
 
 public class PauseMenuWidget : UIMenu
 {
-    [SerializeField] SceneData mainMenuScene;
     [SerializeField] UIWidgetData optionsMenuWidgetData;
     
+    [Header("System")]
+    [SerializeField] SceneData mainMenuScene;
     [SerializeField] SceneChannel requestChangeSceneChannel;
     [SerializeField] UIWidgetDataChannel requestLoadUIWidgetChannel;
     [SerializeField] UIWidgetDataChannel requestUnLoadUIWidgetChannel;
@@ -18,6 +38,84 @@ public class PauseMenuWidget : UIMenu
     {
         base.Teardown();
     }
+
+    public void OnClickMainTab(PauseMenuMainTabs tab)
+    {
+        int _tab = (int) tab;
+        
+        StartCoroutine(Delay(MainTabButton, _tab, 0.1f));
+    }
+
+    private void MainTabButton(int _tab)
+    {
+        PauseMenuMainTabs tab = (PauseMenuMainTabs) _tab;
+    }
+
+    // Journal ============================
+
+    public void OnClickJournalTab(PauseMenuJournalTabs tab)
+    {
+        int _tab = (int) tab;
+        
+        StartCoroutine(Delay(JournalTabButton, _tab, 0.1f));
+    }
+
+    private void JournalTabButton(int _tab)
+    {
+        PauseMenuInventoryTabs tab = (PauseMenuInventoryTabs) _tab;
+    }
+
+    // Equipment ============================
+
+    public void OnClickEquipmentTab(int partyIndex)
+    {
+        StartCoroutine(Delay(EquipmentTabButton, partyIndex, 0.1f));
+    }
+
+    private void EquipmentTabButton(int partyIndex)
+    {
+
+    }
+
+    // Stats ============================
+
+    public void OnClickStatsTab(int partyIndex)
+    {
+        StartCoroutine(Delay(StatsTabButton, partyIndex, 0.1f));
+    }
+
+    private void StatsTabButton(int partyIndex)
+    {
+
+    }
+
+    // Abilities ============================
+
+    public void OnClickAbilitiesTab(int partyIndex)
+    {
+        StartCoroutine(Delay(AbilitiesTabButton, partyIndex, 0.1f));
+    }
+
+    private void AbilitiesTabButton(int partyIndex)
+    {
+
+    }
+
+    // Inventory ============================
+
+    public void OnClickInventoryTab(PauseMenuInventoryTabs tab)
+    {
+        int _tab = (int) tab;
+        
+        StartCoroutine(Delay(InventoryTabButton, _tab, 0.1f));
+    }
+
+    private void InventoryTabButton(int tab)
+    {
+
+    }
+
+    // System ============================
 
     public void OnClickResume()
     {
--- Assets/Scripts/Utilities/GlobalDelegates.cs
+++ Assets/Scripts/Utilities/GlobalDelegates.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class GlobalDelegates
 {
@@ -8,7 +9,9 @@ public class GlobalDelegates
 
 public delegate void VoidDelegate();
 public delegate void IntDelegate(int value);
+public delegate void IntTypeDelegate(int value, Type type);
 public delegate void FloatDelegate(float value);
 public delegate void BoolDelegate(bool value);
 public delegate void Vector2Delegate(Vector2 value);
 public delegate void Vector3Delegate(Vector3 value);
+public delegate void ObjectDelegate(object obj);
